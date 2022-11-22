8bc525efc425d09fa546cbb7aa094015c2e3d9fd
odunayo22 <shakhordun@gmail.com>
2022-11-22 02:00:40 -0800

    changes-handlers

--- handlers.c
+++ handlers.c
@@ -4,14 +4,13 @@ unsigned char handle_flags(const char *flag, char *index);
 unsigned char handle_length(const char *modifier, char *index);
 int handle_width(va_list args, const char *modifier, char *index);
 int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int handle_specifiers(const char *specifiers, va_list, buffer_t *,
-int, unsigned char);
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+	unsigned char, int, int, unsigned char);
 
 /**
  * handle_flags - Matches flags with corresponding values.
  * @flag: A pointer to a potential string of flags.
  * @index: An index counter for the original format string.
- *
  * Return: If flag characters are matched - a corresponding value.
  * Otherwise - 0.
  */
@@ -53,7 +52,6 @@ return (ret);
  * handle_length - Matches length modifiers with their corresponding value.
  * @modifier: A pointer to a potential length modifier.
  * @index: An index counter for the original format string.
- *
  * Return: If a lenth modifier is matched - its corresponding value.
  * Otherwise - 0.
  */
@@ -143,7 +141,7 @@ while ((*modifier >= '0' && *modifier <= '9') ||
 
 if (*modifier == '*')
 {
-rg(args, int);
+value = va_arg(args, int);
 if (value <= 0)
 return (0);
 return (value);
@@ -157,7 +155,7 @@ modifier++;
 return (value);
 }
 
-/**
+/*
  * handle_specifiers - Matches a conversion specifier with
  * a corresponding conversion function.
  * @specifier: A pointer to a potential conversion specifier.
@@ -165,8 +163,8 @@ return (value);
  * Return: If a conversion function is matched - a pointer to the function.
  * Otherwise - NULL.
  */
-unsigned int handle_specifiers(const char *specifier)(va_list, buffer_t *,
-int, unsigned char)
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+	unsigned char, int, int, unsigned char)
 {
 int i;
 converter_t converters[] = {
