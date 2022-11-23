989d32228bfaa8b358e22c0794682986b9480a66
Emmanuel <nwogwugwuemmanuel2@gmail.com>
2022-11-22 17:02:48 -0800

    printf

--- print_alpha.c
+++ print_alpha.c
@@ -8,16 +8,15 @@
  * if a flag is passed to _printf
  * Return: number of char printed
  */
-
 int print_string(va_list l, flags_t *f)
 {
-	char *s = va_arg(l, char *);
+char *s = va_arg(l, char *);
 
-	(void)f;
+(void)f;
 
-	if (!s)
-        s = "(null)";
-    	return (_puts(s));
+if (!s)
+s = "(null)";
+return (_puts(s));
 }
 
 /**
@@ -27,10 +26,9 @@ int print_string(va_list l, flags_t *f)
  * if a flag is passed to _printf
  * Return: number of char printed
  */
-
 int print_char(va_list l, flags_t *f)
 {
-       	(void)f;
-        _putchar(va_arg(l, int));
-    	return (1);
+(void)f;
+_putchar(va_arg(l, int));
+return (1);
 }
--- print_bases.c
+++ print_bases.c
@@ -10,7 +10,6 @@
  * number into the correct base and returns it as a string
  * Return: the number of char printed
  */
-
 int print_hex(va_list l, flags_t *f)
 {
 unsigned int num = va_arg(l, unsigned int);
@@ -18,7 +17,7 @@ char *str = convert(num, 16, 1);
 int count = 0;
 
 if (f->hash == 1 && str[0] != '0')
-	count += _puts("0x");
+count += _puts("0x");
 count += _puts(str);
 return (count);
 }
@@ -35,13 +34,14 @@ return (count);
  */
 int print_hex_big(va_list l, flags_t *f)
 {
-       	unsigned int num = va_arg(l, unsigned int);
-        char *str = convert(num, 16, 0);
-	int count = 0;
-		if (f->hash == 1 && str[0] != '0')
-		        count += _puts("0X");
-		count += _puts(str);
-	        return (count);
+unsigned int num = va_arg(l, unsigned int);
+char *str = convert(num, 16, 0);
+int count = 0;
+
+if (f->hash == 1 && str[0] != '0')
+count += _puts("0X");
+count += _puts(str);
+return (count);
 }
 
 /**
@@ -53,14 +53,13 @@ int print_hex_big(va_list l, flags_t *f)
  * number into the correct base and returns it as a string
  * Return: the number of char printed
  */
-
 int print_binary(va_list l, flags_t *f)
 {
-	unsigned int num = va_arg(l, unsigned int);
-        char *str = convert(num, 2, 0);
+unsigned int num = va_arg(l, unsigned int);
+char *str = convert(num, 2, 0);
 
-	(void)f;
-	return (_puts(str));
+(void)f;
+return (_puts(str));
 }
 
 /**
@@ -72,15 +71,14 @@ int print_binary(va_list l, flags_t *f)
  * number into the correct base and returns it as a string
  * Return: the number of char printed
  */
-
 int print_octal(va_list l, flags_t *f)
 {
-	 unsigned int num = va_arg(l, unsigned int);
-	 char *str = convert(num, 8, 0);
-         int count = 0;
+unsigned int num = va_arg(l, unsigned int);
+char *str = convert(num, 8, 0);
+int count = 0;
 
-    	 if (f->hash == 1 && str[0] != '0')
-             count += _putchar('0');
-         count += _puts(str);
-	 return (count);
+if (f->hash == 1 && str[0] != '0')
+count += _putchar('0');
+count += _puts(str);
+return (count);
 }
